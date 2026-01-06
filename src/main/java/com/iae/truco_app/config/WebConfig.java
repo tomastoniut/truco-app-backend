20e1f6ddf888970550fec791d258470c5d3494b3
Tomas Toniut <tomastoniut01@gmail.com>
2026-01-06 20:21:45 -0300

    .

--- src/main/java/com/iae/truco_app/config/WebConfig.java
+++ src/main/java/com/iae/truco_app/config/WebConfig.java
@@ -4,19 +4,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                    "http://localhost:5173", // Desarrollo local (Vite)
+                // Usamos patterns que es más robusto para validación con credentials
+                .allowedOriginPatterns(
+                    "http://localhost:5173",
                     "http://trucoapp.mardelplatadev.cloud",
                     "https://trucoapp.mardelplatadev.cloud"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true); // Importante si manejas sesiones o cookies
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
